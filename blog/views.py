ff206ea5fa1a77731215b0550b3df3767c612ec4
nkushh <mugunapiero@gmail.com>
2016-08-12 15:34:40 +0300

    Third commit with changes in views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from .models import Post
 
 # Create your views here.
 def allBlogs(request):
-	posts = Post.objects.all()
+	posts = Post.objects.filter(published_on__lte=timezone.now()).order_by('created_on')
 	return render(request, 'blog/index.html', {'posts':posts})
