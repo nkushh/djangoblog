d50bff1f608b9c3fbd8385e2be92c539eaf4ba25
nkushh <mugunapiero@gmail.com>
2016-08-12 15:00:49 +0300

    Second commit with changes in views and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+def allBlogs(request):
+	posts = Post.objects.all()
+	return render(request, 'blog/index.html', {'posts':posts})
