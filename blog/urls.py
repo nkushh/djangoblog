9a4dd25ec479416115fd9257fed1bbf836e005c0
nkushh <mugunapiero@gmail.com>
2016-08-15 21:02:08 +0300

    Login module was added

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from . import views
 
+
 urlpatterns = [
 	url(r'^$', views.allBlogs, name="index"),
 	url(r'^post/(?P<pk>\d+)/$', views.postDetails, name="post-details"),
@@ -9,4 +10,8 @@ urlpatterns = [
 	url(r'^posts/$', views.posts, name="posts"),
 	url(r'^posts/new/$', views.newPost, name="new-post"),
 	url(r'^posts/(?P<pk>\d+)/edit/$', views.editPost, name="edit-post"),
+	url(r'^posts/drafts/$', views.allDrafts, name="all-drafts"),
+	url(r'^posts/published/$', views.allPublished, name="all-published"),
+	url(r'^posts/publish/(?P<pk>\d+)/$', views.publishPost, name="publish-post"),
+	url(r'^posts/delete/(?P<pk>\d+)/$', views.deletePost, name="delete-post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-# Create your views here.
+
+# Front-end
 def allBlogs(request):
 	posts = Post.objects.filter(published_on__lte=timezone.now()).order_by('-created_on')
 	return render(request, 'blog/index.html', {'posts':posts})
@@ -12,23 +15,28 @@ def postDetails(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/blog_post.html', {'post':post})
 
+
+
+# Dashboard
+@login_required
 def dashboard(request):
 	posts = Post.objects.all()
 	return render(request, 'blog/dashboard.html', {'posts':posts})
 
+@login_required
 def newPost(request):
 	if request.method=="POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_on = timezone.now()
 			post.save()
 			return redirect('post_details', pk=post.pk)
 	else:
 		form = PostForm()
 	return render(request, 'blog/new_post.html', {'form':form})
 
+@login_required
 def editPost(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method=="POST":
@@ -43,11 +51,50 @@ def editPost(request, pk):
 		form = PostForm(instance=post)
 	return render(request, 'blog/new_post.html', {'form':form})
 
+@login_required
 def post_details(request, pk):
 	post = Post.objects.get(pk=pk)
 	return render(request, 'blog/post_details.html', {'post':post})
 
-
+@login_required
 def posts(request):
 	posts = Post.objects.all()
 	return render(request, 'blog/posts.html', {'posts':posts})
+
+@login_required
+def allDrafts(request):
+	posts = Post.objects.filter(published_on__isnull=True).order_by('-created_on')
+	return render(request, 'blog/posts.html', {'posts':posts})
+
+@login_required
+def allPublished(request):
+	posts = Post.objects.filter(published_on__lte=timezone.now()).order_by('-created_on')
+	return render(request, 'blog/posts.html', {'posts':posts})
+
+@login_required
+def publishPost(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_details', pk=pk)
+
+@login_required
+def deletePost(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('posts')
+
+def loginUser(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            return render(request, 'blog/login.html')
+    else:
+        return render(request, 'blog/login.html')
+
+def logUserOut(request):
+	logout(request)
